eb0058a748e0e548f57aebc16558df103d930764
eukeo <sixsblooms@gmail.com>
2022-06-10 12:14:49 -0700

    weather api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const apiKey = "b43ed88f87ec4c01a52180535221006";
+
+  const [weatherData, setWeatherData] = useState([{}]);
+  const [city, setCity] = useState("");
+
+  const getWeather = (event) => {
+    if (event.key == "Enter") {
+      fetch(
+        `http://api.weatherapi.com/v1/current.json?key=b43ed88f87ec4c01a52180535221006&q=${city}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setWeatherData(data);
+          setCity("");
+        });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Search a Location</h1>
+      <input
+        type="text"
+        placeholder="Enter City"
+        onChange={(e) => setCity(e.target.value)}
+        value={city}
+        onKeyPress={getWeather}
+      ></input>
+
+      {typeof weatherData.location === "undefined" ? (
+        <div>
+          <p>Enter a Location!</p>
+        </div>
+      ) : (
+        <div>
+          <h1>{weatherData.location.name}</h1>
+          <h4>{weatherData.location.country}</h4>
+          <h4>{weatherData.current.temp_f}°F</h4>
+        </div>
+      )}
     </div>
   );
 }
